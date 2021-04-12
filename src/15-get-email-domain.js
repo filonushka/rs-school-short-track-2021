76152311f5a052360659a222b82501dcce3a8da1
filonushka <lero4ka.filonova@mail.ru>
2021-04-13 01:10:06 +0300

    feat: finish 15-get-email-domain

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,14 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const arr = [];
+  for (let i = 0; i < email.length; i++) {
+    if (email[i] === '@') {
+      arr.push(i);
+    }
+  }
+  return email.substring(arr[arr.length - 1] + 1);
 }
 
 module.exports = getEmailDomain;
